adbc8b4b2ad4e739c68bfee22a4fe60bf7112b3f
Andrey Urusov <drobus@gmail.com>
2014-11-08 10:01:22 +0300

    Enter time delay enerator added

--- python/barber.py
+++ python/barber.py
@@ -4,8 +4,8 @@ class EventEnter(object):
     def __init__(self, name):
         self.name = name
 
-    def run(self, model):
-        model.scheduler.planning(model.time_now + 0, self)
+    def run(self, simulator, model):
+        simulator.scheduler.planning(simulator.time_now + model.get_enter_time_delay.next(), self)
 
         if model.barber.state == Model.Barber.State.BUSY:
             model.queue += 1
@@ -24,6 +24,20 @@ class Model(object):
         def __init__(self):
             self.state = Model.Barber.State.FREE
 
+    def __init__(self):
+        self.event_enter = EventEnter('enter')
+        self.barber = Model.Barber()
+        self.queue = 0
+        self.get_enter_time_delay = Model.__get_enter_time_delay__()
+
+    @staticmethod
+    def __get_enter_time_delay__():
+        enter_time_delay = [14, 13, 17, 15, 16, 15, 14, 22, 19]
+        for delay in enter_time_delay:
+            yield delay
+
+
+class Simulator(object):
     class Scheduler(object):
 
         class Item(object):
@@ -41,26 +55,14 @@ class Model(object):
             self.__events__ = []
 
         def planning(self, time, event):
-            self.__events__.append(Model.Scheduler.Item(time, event))
+            self.__events__.append(Simulator.Scheduler.Item(time, event))
 
     def __init__(self):
-        self.event_enter = EventEnter('enter')
-        self.barber = Model.Barber()
-        self.queue = 0
-        self.scheduler = Model.Scheduler()
         self.time_now = 0.0
+        self.scheduler = Simulator.Scheduler()
 
 model = Model()
-model.event_enter.run(model)
-print model.scheduler.__events__
-
-def qqq():
-    enter_time_delay = [14, 13, 17, 15, 16, 15, 14, 22, 19]
-    for delay in enter_time_delay:
-        print 'a'
-        yield delay
-        print 'b'
-
-for i in qqq():
-    print i
-    print 'c'
+simulator = Simulator()
+model.event_enter.run(simulator, model)
+model.event_enter.run(simulator, model)
+print simulator.scheduler.__events__
