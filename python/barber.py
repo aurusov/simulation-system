5f24cc36b1e20b456675984e799edd5c19b06fda
Andrey Urusov <drobus@gmail.com>
2016-12-29 12:57:55 +0300

    Замена self на что-то читаемое

--- python/barber.py
+++ python/barber.py
@@ -1,10 +1,10 @@
 from enum import Enum
 
-class EventEnter(object):
-    def __init__(self, name):
-        self.name = name
+class EventEnter:
+    def __init__(event, name):
+        event.name = name
 
-    def run(self, simulator, model):
+    def run(event, simulator, model):
         simulator.scheduler.planning(simulator.time_now + model.get_enter_time_delay.next(), model.event_enter)
 
         if model.barber.state == Model.Barber.State.BUSY:
@@ -16,11 +16,11 @@ class EventEnter(object):
             simulator.scheduler.planning(simulator.time_now + model.get_haircut_duration.next(), model.event_finish)
             return
 
-class EventFinish(object):
-    def __init__(self, name):
-        self.name = name
+class EventFinish:
+    def __init__(event, name):
+        event.name = name
 
-    def run(self, simulator, model):
+    def run(event, simulator, model):
         if model.queue > 0:
             model.queue -= 1
             simulator.scheduler.planning(simulator.time_now + model.get_haircut_duration.next(), model.event_finish)
@@ -30,22 +30,22 @@ class EventFinish(object):
             model.barber.state = Model.Barber.State.FREE
 
 
-class Model(object):
+class Model:
 
-    class Barber(object):
+    class Barber:
         class State(Enum):
             FREE = 1
             BUSY = 2
-        def __init__(self):
-            self.state = Model.Barber.State.FREE
+        def __init__(barber):
+            barber.state = Model.Barber.State.FREE
 
-    def __init__(self):
-        self.event_enter = EventEnter('enter')
-        self.event_finish = EventFinish('finish')
-        self.barber = Model.Barber()
-        self.queue = 0
-        self.get_enter_time_delay = Model.__get_enter_time_delay__()
-        self.get_haircut_duration = Model.__get_haircut_duration__()
+    def __init__(model):
+        model.event_enter = EventEnter('enter')
+        model.event_finish = EventFinish('finish')
+        model.barber = Model.Barber()
+        model.queue = 0
+        model.get_enter_time_delay = Model.__get_enter_time_delay__()
+        model.get_haircut_duration = Model.__get_haircut_duration__()
 
     @staticmethod
     def __get_enter_time_delay__():
@@ -60,39 +60,39 @@ class Model(object):
             yield duration
 
 
-class Simulator(object):
+class Simulator:
 
-    class Scheduler(object):
+    class Scheduler:
 
-        class Item(object):
-            def __init__(self, time, event):
-                self.time = time
-                self.event = event
+        class Item:
+            def __init__(item, time, event):
+                item.time = time
+                item.event = event
 
-            def __repr__(self):
-                return str(self)
+            def __repr__(item):
+                return str(item)
 
-            def __str__(self):
-                return str(self.time) + ', ' + self.event.name
+            def __str__(item):
+                return str(item.time) + ', ' + item.event.name
 
-        def __init__(self):
-            self.__items__ = []
+        def __init__(scheduler):
+            scheduler.__items__ = []
 
-        def planning(self, time, event):
-            self.__items__.append(Simulator.Scheduler.Item(time, event))
+        def planning(scheduler, time, event):
+            scheduler.__items__.append(Simulator.Scheduler.Item(time, event))
 
-    def __init__(self):
-        self.time_now = 0.0
-        self.scheduler = Simulator.Scheduler()
+    def __init__(simulator):
+        simulator.time_now = 0.0
+        simulator.scheduler = Simulator.Scheduler()
 
-    def run(self, model):
-        while self.scheduler.__items__:
-            self.scheduler.__items__.sort(key=lambda item: item.time)
-            item = self.scheduler.__items__[0]
-            self.scheduler.__items__.remove(item)
-            self.time_now = item.time
-            item.event.run(self, model)
-            print 'time = %f, queue = %d, barber = %s, future_events_list = %s' % (self.time_now, model.queue, model.barber.state, self.scheduler.__items__)
+    def run(simulator, model):
+        while simulator.scheduler.__items__:
+            simulator.scheduler.__items__.sort(key=lambda item: item.time)
+            item = simulator.scheduler.__items__[0]
+            simulator.scheduler.__items__.remove(item)
+            simulator.time_now = item.time
+            item.event.run(simulator, model)
+            print 'time = %f, queue = %d, barber = %s, future_events_list = %s' % (simulator.time_now, model.queue, model.barber.state, simulator.scheduler.__items__)
 
 
 model = Model()
